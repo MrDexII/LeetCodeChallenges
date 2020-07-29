91e87adb0516e13d5b47b982e0d66f51b9c98d59
MrDexII <free.andrew.a@gmail.com>
2020-07-29 15:01:07 +0200

    Move zeroes

---
+++ src/main/java/Arrays/MoveZeroes.java
@@ -0,0 +1,58 @@
+package Arrays;
+
+public class MoveZeroes {
+
+    //Space complexity O(1)
+    //Time complexity  O(n^2)
+    public void moveZeroes(int[] nums) {
+        int numberOfZeros = 0;
+
+        for (int i = 0; i < nums.length - numberOfZeros; i++) {
+            if (nums[i] == 0) {
+                int index = i;
+
+                for (int j = index + 1; j < nums.length - numberOfZeros; j++)
+                    index = swap(nums, index, j);
+
+                numberOfZeros++;
+                i--;
+            }
+        }
+    }
+    //Space complexity O(1)
+    //Time complexity  O(n)
+    public void moveZeroes2(int[] nums) {
+        int start  = -1;
+        int end = 0;
+        for(end = 0; end<nums.length; end++){
+            if(nums[end]!=0 && start!=-1){
+                swapVoid(nums, start, end);
+                start++;
+            }else if(nums[end]==0 && start==-1){
+                start = end;
+            }
+        }
+    }
+
+    private int swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i++] = nums[j];
+        nums[j] = temp;
+        return i;
+    }
+
+    private void swapVoid(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes moveZeroes = new MoveZeroes();
+
+        int[] nums = {0,1,0,3,12};
+
+        moveZeroes.moveZeroes2(nums);
+
+    }
+}
