cce689836144d55a954013eee12563bb8b8e190a
MrDexII <free.andrew.a@gmail.com>
2020-08-08 09:18:32 +0200

    Memoization

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,15 +1,8 @@
 public class Fibonacci {
-
-    public static void main(String[] args) {
-        int number = 10;
-        fibonacci(number);
-    }
-
-    public static void fibonacci(int i) {
+    public void fibonacci(int i) {
         long a = 0;
         long b = 1;
         long result = 0;
-
 
         if (i == 0)
             System.out.print(i);
@@ -25,14 +18,29 @@ public class Fibonacci {
         }
     }
 
-    public static long fibonacciRecursion(int i) {
+    public long fibonacciRecursion(int i) {
         if (i == 0)
             return 0;
         else if (i == 1)
             return 1;
         else
             return fibonacciRecursion(i - 1) + fibonacciRecursion(i - 2);
+    }
 
+    public long fibonacciRecursionMemoization(int i, long[] memo) {
+        if (i == 0)
+            return 0;
+        else if (i == 1)
+            return 1;
+        else if (memo[i - 1] == 0)
+            memo[i - 1] = (fibonacciRecursionMemoization(i - 1, memo) + fibonacciRecursionMemoization(i - 2, memo));
+        return memo[i - 1];
+    }
+
+    public static void main(String[] args) {
+        int number = 10;
+        Fibonacci fibonacci = new Fibonacci();
+
+        System.out.println(fibonacci.fibonacciRecursionMemoization(number, new long[number]));
     }
 }
-//https://www.java67.com/2016/05/fibonacci-series-in-java-using-recursion.html
