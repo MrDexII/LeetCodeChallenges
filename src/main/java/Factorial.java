582f5b58bee8a2e94fa09b9a2f584b241461a5f6
MrDexII <free.andrew.a@gmail.com>
2021-07-23 13:34:54 +0200

    functional factorial

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,9 +1,12 @@
+import java.util.stream.LongStream;
+
 public class Factorial {
     public static void main(String[] args) {
-        int number = 20;
+        int number = 10;
         System.out.println(factorial(number));
         System.out.println(factorialRecursive(number));
-        System.out.println(Long.MAX_VALUE);
+        System.out.println(factorialDeclarative(number));
+        //System.out.println(Long.MAX_VALUE);
     }
 
     private static long factorial(int i) {
@@ -16,9 +19,13 @@ public class Factorial {
     }
 
     private static long factorialRecursive(int i) {
-        if (i == 0 || i == 1)
-            return 1;
-        else
-            return i * factorialRecursive(i - 1);
+        if (i == 0 || i == 1) return 1;
+        return i * factorialRecursive(i - 1);
+    }
+
+    private static long factorialDeclarative(int number) {
+        return LongStream
+                .range(1, number + 1)
+                .reduce(1, (base, multi) -> base * multi);
     }
 }
