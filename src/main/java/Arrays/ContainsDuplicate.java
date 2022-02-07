9c1d7a7f18abc951bcb0d274b069f6cab1acaacc
MrDexII <free.andrew.a@gmail.com>
2022-02-07 14:51:33 +0100

    Contains Duplicate

--- src/main/java/Arrays/ContainsDuplicate.java
+++ src/main/java/Arrays/ContainsDuplicate.java
@@ -5,30 +5,44 @@ import java.util.Set;
 
 public class ContainsDuplicate {
 
-    public static boolean containsDuplicateBruteForce(int[] nums) {
+    // time complexity O(n^2)
+    // space complexity O(1)
+    public static boolean containsDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) return true;
             }
         }
         return false;
     }
 
-    public static boolean containsDuplicate(int[] nums) {
-        if (nums.length <= 1) return false;
-        Set<Integer> integerSet = new HashSet<>();
-        for (int number : nums)
-            if(!integerSet.add(number)) return true;
+    // time complexity O(n)
+    // space complexity O(n)
+    public static boolean containsDuplicate2(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsSet.contains(nums[i])) return true;
+            else {
+                numsSet.add(nums[i]);
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        //int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
-        //int[] nums = {1};
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] nums4 = {1, 1};
+        int[] nums5 = {1, 2};
+        int[] nums6 = {1};
 
-        System.out.println(containsDuplicate(nums));
+        System.out.println(containsDuplicate2(nums1));
+        System.out.println(containsDuplicate2(nums2));
+        System.out.println(containsDuplicate2(nums3));
+        System.out.println(containsDuplicate2(nums4));
+        System.out.println(containsDuplicate2(nums5));
+        System.out.println(containsDuplicate2(nums6));
+
     }
 }
